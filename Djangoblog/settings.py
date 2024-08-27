ff891e4bdb824745394ec2b78a46bbdbdb620b5b
Harry <prince11290824@gmail.com>
2024-08-27 23:22:23 +0800

    try push heroku

--- Djangoblog/settings.py
+++ Djangoblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z@ictmyb&+@m*8o7kc$8s#8cx2-x&r6s_j-%_oyo0_wxy!z%ep
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['git.heroku.com/django-0827.git']
 
 
 # Application definition
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Djangoblog.urls'
